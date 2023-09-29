22a679a7ac964822956a15b1eba3773de28719ab
Mehedi Hasan <mehedisn3@gmail.com>
2023-09-30 00:13:07 +0600

    Start Learning

--- 01_Basic_SQL_Server_Introduction.sql
+++ 01_Basic_SQL_Server_Introduction.sql
@@ -1,2 +1,38 @@
 
 -- **********************  Basic Introduction of SQL Server *****************************
+
+/* 
+    SQL Server is a relational database management system that is used for data storage and retrieval.
+    SQL Server provides a wide range of features and capabilities for data storage and retrieval.
+    
+    SQL server Management studio is just client tool that connect to 
+    a central database
+
+    4 Developer  by SSMS => SQL Server Management Studio 
+
+        client machine 1              client machine 2           
+                     \                /
+                      Database Server
+                     /              \
+       client machine 3              client machine 4
+
+
+    Through the IP address we can connect to Database 
+    
+    Database can be local machine or remote 
+
+
+    *** Database is collection number of tables
+
+    [ we see some databse are pre makeking into Database insides (System Database) master, model, msdb so on
+    This are functionality insure in our database ]
+
+    Next we make some user define database 
+    ....
+    ...
+    ..
+    .  
+
+
+
+*/
